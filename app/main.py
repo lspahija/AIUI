8cca03987db2cccd6f993322f4b7a232e5e85aa6
lspahija <lspahijaoec@gmail.com>
2023-03-30 09:43:57 -0500

    specify language to use in the prompt

--- app/main.py
+++ app/main.py
@@ -87,7 +87,7 @@ async def get_completion(user_prompt, conversation_thus_far):
     start_time = time.time()
     messages = [
         {"role": "system",
-         "content": "You are a helpful assistant with a voice interface. Keep your responses succinct since the user is interacting with you through a voice interface. Your response should be a few sentences at most. Give your answer in the same language as the immediately preceding prompt."}
+         "content": f"You are a helpful assistant with a voice interface. Keep your responses succinct since the user is interacting with you through a voice interface. Your responses should be a few sentences at most. Always provide your responses in the language that corresponds to the ISO-639-1 code: {LANGUAGE}."}
     ]
     messages.extend(get_additional_initial_messages())
     messages.extend(json.loads(base64.b64decode(conversation_thus_far)))
@@ -107,7 +107,7 @@ def get_additional_initial_messages():
     match AI_COMPLETION_MODEL:
         case "gpt-3.5-turbo":
             return [{"role": "user",
-                     "content": "Make sure you strictly give your answer in the same language as the immediately preceding prompt."}]
+                     "content": f"Make sure you always strictly provide your responses in the language that corresponds to the ISO-639-1 code: {LANGUAGE}."}]
         case _:
             return []
 
