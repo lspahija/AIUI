a35aaf25f5710b90cddd3951889ae76f1ba38f4a
lspahija <lspahijaoec@gmail.com>
2023-05-06 02:57:36 +0200

    delete only own containers

--- run.sh
+++ run.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 set -e
 
-if [ "$(docker ps -a -q)" ]; then
-    docker rm -f $(docker ps -a -q)
+CONTAINER_LABEL="created_by=aiui_script"
+
+# Remove containers with the specified label
+if [ "$(docker ps -a -q -f "label=$CONTAINER_LABEL")" ]; then
+    docker rm -f $(docker ps -a -q -f "label=$CONTAINER_LABEL")
 fi
 
 docker build -t aiui .
 
 if [ "$1" == "gTTS" ]; then
-    docker run -d -e OPENAI_API_KEY="${OPENAI_API_KEY}" -e TTS_PROVIDER="gTTS" -p 8000:80 aiui
+    docker run -d -e OPENAI_API_KEY="${OPENAI_API_KEY}" -e TTS_PROVIDER="gTTS" -p 8000:80 --label "$CONTAINER_LABEL" aiui
 elif [ "$1" == "ELEVENLABS" ]; then
-    docker run -d -e OPENAI_API_KEY="${OPENAI_API_KEY}" -e TTS_PROVIDER="ELEVENLABS" -e ELEVENLABS_API_KEY="${ELEVENLABS_API_KEY}" -p 8000:80 aiui
+    docker run -d -e OPENAI_API_KEY="${OPENAI_API_KEY}" -e TTS_PROVIDER="ELEVENLABS" -e ELEVENLABS_API_KEY="${ELEVENLABS_API_KEY}" -p 8000:80 --label "$CONTAINER_LABEL" aiui
 elif [ "$1" == "STREAMELEMENTS" ]; then
-    docker run -d -e OPENAI_API_KEY="${OPENAI_API_KEY}" -e TTS_PROVIDER="STREAMELEMENTS" -p 8000:80 aiui
+    docker run -d -e OPENAI_API_KEY="${OPENAI_API_KEY}" -e TTS_PROVIDER="STREAMELEMENTS" -p 8000:80 --label "$CONTAINER_LABEL" aiui
 else
     echo "Invalid argument. Please provide either 'gTTS', 'ELEVENLABS' or 'STREAMELEMENTS'."
     exit 1
