86b54da1169e3fa204cfd852c61e1258420b705b
lspahija <lspahijaoec@gmail.com>
2023-06-07 12:30:11 -0500

    cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,52 @@
 import {useMicVAD} from "@ricky0123/vad-react";
 import {useEffect, useRef, useState} from "react";
-import Canvas from "./Canvas.tsx";
 import RotateLoader from "react-spinners/RotateLoader";
 import {onMisfire, onSpeechEnd, onSpeechStart} from "./SpeechManager.ts";
 import {particleActions} from "./ParticleManager.ts";
 
+const useCanvas = (draw) => {
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+        if (!canvasRef.current) return;
+
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+        let animationFrameId;
+
+        const render = () => {
+            draw(context, canvas.width, canvas.height, canvas.width / 2, canvas.height / 2);
+            animationFrameId = window.requestAnimationFrame(render);
+        };
+
+        render();
+
+        return () => {
+            window.cancelAnimationFrame(animationFrameId);
+        }
+    }, [draw]);
+
+    return canvasRef;
+}
+
+
+const useMicVADWrapper = (options, onLoadingChange) => {
+    const micVAD = useMicVAD(options);
+    const loadingRef = useRef(micVAD.loading);
+
+    useEffect(() => {
+        if (loadingRef.current !== micVAD.loading) {
+            onLoadingChange(micVAD.loading);
+            loadingRef.current = micVAD.loading;
+        }
+    });
+
+    return micVAD;
+}
+
 const App = () => {
-    const [loading, setLoading] = useState(true)
+    const [loading, setLoading] = useState(true);
+    const canvasRef = useCanvas(particleActions.draw);
 
     useMicVADWrapper({
         preSpeechPadFrames: 5,
@@ -14,46 +54,32 @@ const App = () => {
         negativeSpeechThreshold: 0.75,
         minSpeechFrames: 4,
         startOnLoad: true,
-        onSpeechStart: async () => onSpeechStart(),
-        onSpeechEnd: async audio => onSpeechEnd(audio),
-        onVADMisfire: () => onMisfire()
-    }, loading => setLoading(loading))
+        onSpeechStart,
+        onSpeechEnd,
+        onVADMisfire: onMisfire
+    }, setLoading);
 
     return (
         loading
-            ?
-            <div style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "100vh",
-                width: "100vw",
-            }}>
-                <RotateLoader
-                    loading={loading}
-                    color={"#27eab6"}
-                    aria-label="Loading Spinner"
-                    data-testid="loader"
-                />
-            </div>
-            :
-            <Canvas draw={particleActions.draw}/>
+            ? (
+                <div style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "100vh",
+                    width: "100vw",
+                }}>
+                    <RotateLoader
+                        loading={loading}
+                        color={"#27eab6"}
+                        aria-label="Loading Spinner"
+                        data-testid="loader"
+                    />
+                </div>
+            ) : (
+                <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight}/>
+            )
     );
 }
 
-function useMicVADWrapper(options, onLoadingChange) {
-    const micVAD = useMicVAD(options)
-    const loadingRef = useRef(micVAD.loading)
-
-    useEffect(() => {
-        if (loadingRef.current !== micVAD.loading) {
-            loadingRef.current = micVAD.loading
-            onLoadingChange(micVAD.loading)
-        }
-    });
-
-    return micVAD
-}
-
-
-export default App
+export default App;
