86fe7143f8ce8ec16f9b1a8ef03fd10c8f0ae8f5
lspahija <lspahijaoec@gmail.com>
2023-06-08 13:10:09 -0500

    cleanup

--- frontend/src/hooks/useMicVADWrapper.ts
+++ frontend/src/hooks/useMicVADWrapper.ts
@@ -1,9 +1,20 @@
 import {useEffect, useRef} from "react";
 import {useMicVAD} from "@ricky0123/vad-react";
-import {VAD_OPTIONS} from "../constants.ts";
+import {onMisfire, onSpeechEnd, onSpeechStart} from "../speech-manager.ts";
 
 export const useMicVADWrapper = (onLoadingChange) => {
-    const micVAD = useMicVAD(VAD_OPTIONS);
+    const micVAD = useMicVAD(
+        {
+            preSpeechPadFrames: 5,
+            positiveSpeechThreshold: 0.90,
+            negativeSpeechThreshold: 0.75,
+            minSpeechFrames: 4,
+            startOnLoad: true,
+            onSpeechStart,
+            onSpeechEnd,
+            onVADMisfire: onMisfire
+        }
+    );
     const loadingRef = useRef(micVAD.loading);
 
     useEffect(() => {
